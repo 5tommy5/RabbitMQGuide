3a69fee4e01162b7798db4d1cf84dff3d4ca8715
Anton Tomchuk <a.tomchuk@winstars.tech>
2023-09-03 16:40:12 +0300

    background service to listen the queue

---
+++ Consumer.Api/Services/RabbitListener.cs
@@ -0,0 +1,22 @@
+﻿using RabbitMQ.Client;
+
+namespace Consumer.Api.Services
+{
+    public class RabbitListener : BackgroundService
+    {
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public RabbitListener()
+        {
+            var factory = new ConnectionFactory { HostName = "localhost" };
+            _connection = factory.CreateConnection();
+            _channel = _connection.CreateModel();
+            _channel.QueueDeclare(queue: "Messages", durable: false, exclusive: false, autoDelete: false, arguments: null);
+        }
+        protected override Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
